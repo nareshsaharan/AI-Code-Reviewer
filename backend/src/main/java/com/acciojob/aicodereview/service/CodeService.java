734740ac4bd1864b35bdecbc8f0f857fafeb69f8
Naresh Saharan <nareshsaharan2016@gmail.com>
2026-01-04 22:20:18 +0530

    fix: added codeVersion table entry for the first code analysis

--- backend/src/main/java/com/acciojob/aicodereview/service/CodeService.java
+++ backend/src/main/java/com/acciojob/aicodereview/service/CodeService.java
@@ -1,7 +1,10 @@
 package com.acciojob.aicodereview.service;
 
 import com.acciojob.aicodereview.model.CodeSubmission;
+import com.acciojob.aicodereview.model.CodeVersion;
 import com.acciojob.aicodereview.repository.CodeSubmissionReporsitory;
+import com.acciojob.aicodereview.repository.CodeVersonRepository;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,9 +14,20 @@ public class CodeService {
     @Autowired
     private CodeSubmissionReporsitory submissionReporsitory;
 
+    @Autowired
+    private CodeVersonRepository versionRepository;
+
     public CodeSubmission createSubmission(CodeSubmission submission) {
 
         CodeSubmission saved = submissionReporsitory.save(submission);
+
+        // create inital version
+        CodeVersion version = new CodeVersion();
+        version.setSubmission(saved);
+        version.setVersionNumber(1);
+        version.setCode(submission.getCode());
+        version.setAnalysis("Pending analysis...");
+        versionRepository.save(version);
 
         return saved;
 
